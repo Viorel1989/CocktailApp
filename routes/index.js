352be1f78661cf5f3418fe4776571843f1f04beb
Viorel <viorel_popa89@yahoo.com>
2022-06-18 16:09:55 +0300

    show data on pages using backend rerquests

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
 var express = require("express");
+const axios = require("axios").default;
 var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Cocktail App" });
+  res.render("index");
+});
+
+router.post("/", function (req, res, next) {
+  const cocktailName = req.body.cocktailName;
+
+  console.log("call cocktail api");
+
+  axios
+    .get("http://www.thecocktaildb.com/api/json/v1/1/search.php", {
+      params: {
+        s: cocktailName,
+        api_key: 1,
+      },
+    })
+    .then(function (response) {
+      let result = [];
+
+      response.data.drinks.forEach((drink) => {
+        const cocktail = `${drink.strDrink}`;
+        result.push(cocktail);
+      });
+      res.render("index", { cocktail: result });
+    })
+
+    .catch(function (error) {
+      console.log(error);
+    });
 });
 
 module.exports = router;
--- routes/mix.js
+++ routes/mix.js
@@ -1,9 +1,29 @@
 var express = require("express");
+const axios = require("axios").default;
 var router = express.Router();
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
-  res.render("mix");
+  axios
+    .get("http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list", {
+      params: {
+        api_key: 1,
+      },
+    })
+    .then(function (response) {
+      console.log(response.data.drinks);
+      let result = [];
+
+      response.data.drinks.forEach((drink) => {
+        const ingredient = `${drink.strIngredient1}`;
+        result.push(ingredient);
+      });
+      res.render("mix", { ingredient: result });
+    })
+
+    .catch(function (error) {
+      console.log(error);
+    });
 });
 
 module.exports = router;
